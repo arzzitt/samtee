452565a4c3217e8c8c66f9a9fe6d2f2dd4c954cd
Arjit <arjitmishra42@gmail.com>
2022-06-27 23:26:06 +0530

    commit

---
+++ lib/apis/Access.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'NetworkHelper.dart';
+
+class access {
+  static String base_url = 'https://santee.phaico.com/wp-json/wc/v3/';
+
+  // Future<dynamic> login(String email, password,) async {
+  //   NetworkHelper helper = NetworkHelper(
+  //     base_url + "login/email",
+  //   );
+  //   var data = await helper.login(email, password);
+
+  //   // Profile prof = Profile.fromJson(data);
+  //   return data;
+  // }
+
+  Future<dynamic> signup(
+      String firstname, lastname, email, password, username) async {
+    NetworkHelper helper = NetworkHelper(base_url + "customers");
+    var data =
+        await helper.signup(firstname, lastname, email, password, username);
+    return data;
+  }
+
+  // Future<dynamic> employeeList(String name) async {
+
+  //   NetworkHelper helper = NetworkHelper(base_url + "admin/search/employee",);
+  //   var data = await helper.employeeList(name);
+
+  //   // Profile prof = Profile.fromJson(data);
+  //   return data;
+  // }
+
+  // Future<dynamic> uploadVisitorImage(String path) async {
+  //   NetworkHelper helper = NetworkHelper(base_url + "upload/img/VisitorImg",);
+  //   var data=await helper.uploadVisitorImage(path);
+  //   return data;
+
+  // }
+
+  // Future<dynamic> visitor(String visitorName, String visitorEmail, String visitorPhone, String company, int employeeId, String? imageurl) async {
+  //   NetworkHelper helper = NetworkHelper(base_url + "visit",);
+  //   var data=await helper.visitor(visitorName,visitorEmail,visitorPhone,company,employeeId,imageurl);
+  //   return data;
+  // }
+
+  // Future<dynamic> delivery(String deliveryPersonName, String deliverPersonPhone, String deliverCompany, int employeeID) async {
+  //   NetworkHelper helper = NetworkHelper(base_url + "delivery",);
+  //   var data=await helper.delivery(deliveryPersonName,deliverPersonPhone,deliverCompany,employeeID);
+  //   return data;
+  // }
+
+  // Future<dynamic> contractor(String visitorName, String visitorEmail, String visitorPhone, String company, int employeeId, String? imageurl) async {
+  //   NetworkHelper helper = NetworkHelper(base_url + "contractor",);
+  //   var data=await helper.contractor(visitorName,visitorEmail,visitorPhone,company,employeeId,imageurl);
+  //   return data;
+  // }
+
+  // Future<dynamic>signout(String? code) async{
+  //   NetworkHelper helper = NetworkHelper(base_url + "signout",);
+  //   var data=await helper.signout(code);
+  //   return data;
+  // }
+
+  // Future<dynamic>attendance(String employeeId, String? imageurl)async {
+  //   NetworkHelper helper = NetworkHelper(base_url + "employee/attendance/mark",);
+  //   var data=await helper.attendance(employeeId,imageurl);
+  //   return data;
+  // }
+
+}
---
+++ lib/apis/NetworkHelper.dart
@@ -0,0 +1,74 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+import 'package:http_parser/http_parser.dart';
+import 'package:login_flow/api/model/customer.dart';
+
+class NetworkHelper {
+  final url;
+
+  NetworkHelper(this.url);
+
+  Dio? dio;
+  BaseOptions option =
+      BaseOptions(connectTimeout: 10000, receiveTimeout: 10000, headers: {
+    'Content-Type': 'application/json',
+
+    // 'Authorization': 'Bearer ${Storage.get_accessToken()}',
+  });
+
+  // Future login(String email, password) async {
+  //   dio = Dio(option);
+  //   try {
+  //     Response? response =
+  //         await dio?.post(url, data: {'email': email, 'password': password});
+  //     LogInApiRes loginApiRes=LogInApiRes.fromJson(response?.data);
+
+  //     if (response?.statusCode == 200 || response?.statusCode == 201) {
+
+  //       // print("access:${loginApiRes.tokens!.accessToken}");
+  //       // print("refresh:${loginApiRes.tokens!.refreshToken}");
+
+  //       print(response?.data);
+
+  //       return response?.data;
+  //     } else {
+  //       return {'success': false, 'message': 'Failed'};
+  //     }
+  //   } on DioError catch (e) {
+  //     print(e.message.toString());
+  //     return {'success': false, 'message': e.message};
+  //   }
+  // }
+
+  Future signup(String firstname, lastname, email, password, username) async {
+    dio = Dio(option);
+    try {
+      Response? response = await dio?.post(url, data: {
+        "first_name": firstname,
+        "last_name": lastname,
+        "email": email,
+        "password": password,
+        "username": username
+      }, queryParameters: {
+        "consumer_key": "ck_994a21efc62a2e77a1a8b645e8c5f3b85d7d37e3",
+        "consumer_secret": "cs_cf1a434e7a13a5795b0baacc7e838bcfc3d4e1bc"
+      });
+      CustomerModel model = CustomerModel.fromJson(response?.data);
+
+      if (response?.statusCode == 200 || response?.statusCode == 201) {
+        print(response?.data);
+
+        return response?.data;
+      } else {
+        return {'success': false, 'message': 'Failed'};
+      }
+    } on DioError catch (e) {
+      print("error: ${e.message.toString()}");
+      return {'success': false, 'message': e.message};
+    }
+  }
+}
